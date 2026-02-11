532cd727a53c8a640b3f600c737cbb9bd7dfb4e1
revaldosteven <revaldosteven1986@gmail.com>
2026-02-11 13:02:37 +0700

    use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
@@ -8,10 +8,7 @@ const config = {
 	preprocess: [
 		vitePreprocess(),
 		mdsvex({
-			extensions: ['.md'],
-			layout: {
-				blog: 'src/lib/components/layout/BlogLayout.svelte'
-			}
+			extensions: ['.md']
 		})
 	],
 	kit: {
